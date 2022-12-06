be2ed2471329ff30c0804350d6c8e2d923868c43
Vothithuyha <116275455+Vothithuyha@users.noreply.github.com>
2022-12-06 15:51:06 +0700

    update bt

---
+++ btthoitrang.js
@@ -0,0 +1,90 @@
+var man=[
+    {
+        id:1,
+        name:"The Cosmo (Den) quan short kaki",
+        code: "TC1025011BA",
+        price: "250.000đ",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVO3tsxcaGjFTp8oYFBHLdWPqSY0BvdUOsiA&usqp=CAU"
+    },
+    {
+        id:2,
+        name:"Quan baggy den sang trong QQ",
+        code:"TC1025001BA",
+        price:"398.000đ",
+        image:"https://cf.shopee.vn/file/6464d23c90ea5ca5a782888abbff08a9 "
+    },
+    {
+        id:3,
+        name:"The cosmo (Den) quan short kaki",
+        code:"TC1025001BA",
+        price:"300.000đ",
+        image:" https://vn-test-11.slatic.net/p/fff9ee9c107a209176e3711eac1ee290.jpg"
+    },
+    {
+        id:4,
+        name:"The cosmo (Den) quan short kaki",
+        code:"TC1025001BA",
+        price:"300.000đ",
+        image:" https://cf.shopee.vn/file/f8f71a038f9baa48ddee565d16bf76a9"
+    }
+]
+var woment=[
+    {
+        id:1,
+        name:"Vay Fashion",
+        code:"TC1025001BA",
+        price:"250.000đ",
+        image:"https://salt.tikicdn.com/cache/280x280/ts/product/5a/b6/0d/3d8d9fd6e182fd9030127592184bab41.png"
+    },
+    {
+        id:2,
+        name:"Ao thun phoi vay ngan",
+        code:"TC1025001BA",
+        price:"398.000đ",
+        image:"https://sakurafashion.vn/upload/sanpham/large/4331-ao-thun-nu-hinh-2-chu-tho-1.jpg"
+    },
+    {
+        id:3,
+        name:"Ao khoac",
+        code:"TC1025001BA",
+        price:"300.000đ",
+        image:"https://lzd-img-global.slatic.net/g/p/545b9fa0352be0363ceefa00e1757749.jpg_720x720q80.jpg_.webp"
+    },
+    {
+        id:4,
+        name:"Vay cong chua",
+        code:"TC1025001BA",
+        price:"300.000đ",
+        image:"https://lzd-img-global.slatic.net/g/p/0f22348be7f686e3f2fa674ca925d21d.jpg_720x720q80.jpg_.webp"
+    }
+]
+function listproducts(){
+    for(var i=0;i<=man.length-1;i++){
+        var demo='<div class="col-3">';
+        demo += '<div class="card" style="width:20rem;">';
+        demo += '<img src="' + man[i].image+'" class="card-img-top" style="height:400px;">';
+        demo += '<div class="card-body">';
+        demo += '<p class="card-text">'+man[i].name +'</p>'
+        demo += '<p class="card-text">'+man[i].price+'</p>';
+        demo += '<a href="#" class="btn btn-primary" onclick="order()">Đặt mua</a>'
+        demo += '</div>';
+        demo += '</div>';
+        demo += '</div>';
+        console.log(demo);
+        document.getElementById("men").innerHTML+=demo;
+    }
+    for(var i=0;i<=woment.length-1;i++){
+        var demo='<div class="col-3">';
+        demo += '<div class="card" style="width:20rem;">';
+        demo += '<img src="' + woment[i].image+'" class="card-img-top" style="height:400px;">';
+        demo += '<div class="card-body">';
+        demo += '<p class="card-text">'+woment[i].name +'</p>'
+        demo += '<p class="card-text">'+woment[i].price+'</p>' ;
+        demo += '<a href="#" class="btn btn-primary" onclick="order()">Đặt mua</a>'
+        demo += '</div>';
+        demo += '</div>';
+        demo += '</div>';
+        console.log(demo);
+        document.getElementById("woman").innerHTML+=demo;
+    }
+}
